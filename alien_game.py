4dd03cf012940b79075641ef0ab00a0a327c2e9b
oyewalemarvellous <marvellousoyewale64@gmail.com>
2025-11-13 17:08:33 +0000

    Add files via upload

--- alien_game.py
+++ alien_game.py
@@ -5,21 +5,27 @@ WIDTH=500
 HEIGHT=500
 TITLE="Alien invasion."
 message=""
+score= "score"
+scores= 0
 Alien=Actor("alien_man")
 def draw():
-    screen.fill("black")
+    screen.fill("black") # pyright: ignore[reportUndefinedVariable]
     Alien.draw()
-    screen.draw.text(message,center=(WIDTH/2,50))
-
+    screen.draw.text(message,center=(WIDTH/2,50)) # pyright: ignore[reportUndefinedVariable]
+    screen.draw.text(score,center=(50,5))
+    screen.draw.text(str(scores),center=(50,50)) # pyright: ignore[reportUndefinedVariable]
 def alien_position():
     Alien.x=random.randint(90,490)
     Alien.y=random.randint(90,490)
 def on_mouse_down(pos):
     global message
+    global scores
     if Alien.collidepoint(pos):
         alien_position()
         message="good shot"
+        scores +=1
     else:
         message="bad shot "
+        scores -=2
 
 pgzrun.go()
