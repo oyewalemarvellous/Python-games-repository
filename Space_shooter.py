a911beacaa7ca4ac4450f3827c900a5ce706f26e
oyewalemarvellous <marvellousoyewale64@gmail.com>
2025-12-09 17:20:01 +0000

    Add files via upload

--- Space_shooter.py
+++ Space_shooter.py
@@ -12,8 +12,20 @@ alien_lazer=[]
 score_name= "Scores :"
 score= 0
 speed= 2
+boss_health= 500
 alien_troops=[]
-game_over = False 
+game_over = False
+is_boss= False
+def final_boss():
+    global is_boss
+    global boss 
+    if score >= 200 and boss is None:
+        boss= Actor("mother_ship")
+        boss.pos= (WIDTH/2,100)
+        is_boss= True
+def boss_movement():
+    boss.y += 2
+
 def alien_fleet():
     for i in range(8):
         for c in range(3):
@@ -27,16 +39,18 @@ def space_craft_collision():
     for alien in alien_troops:
         if alien.colliderect(space_craft):
             game_over = True
-        elif alien.y > 600:
-            game_over = True 
+            """elif alien.y > 600:
+            game_over = True """
         elif score < 0:  
             game_over= True 
+        elif boss_health < 1:
+            game_over=True
 def alien_shoot():
     shooter=random.choice(alien_troops)
     laser=Actor("blue_lazer_beam")
     laser.pos=(shooter.x,shooter.y)
     alien_lazer.append(laser)
-    clock.schedule(alien_shoot,1.0)
+    clock.schedule_interval(alien_shoot,3.0)
 clock.schedule(alien_shoot,3.0)
 def max_score():
     if score > 300:
@@ -57,6 +71,8 @@ def draw():
         lazer.draw()
     screen.draw.text(score_name,center=(25,10))
     screen.draw.text(str(score),center=(65,10))
+    if is_boss:
+        boss.draw()
     max_score()
     for laser in alien_lazer:
         laser.draw()
@@ -76,12 +92,16 @@ def space_craft_wallcollision():
     if space_craft.x < 0:
         space_craft.pos= (550,560)
 def update():
+    global boss
     global score
     global speed
     if  game_over:
         return
     for lazer in lazers:
         lazer.y -=3
+        if is_boss:
+            if boss.colliderect(lazer):
+                boss_health -= 50
         if lazer.y < 0 and lazer in lazers:
             lazers.remove(lazer)
         for alien in alien_troops:
@@ -95,10 +115,15 @@ def update():
             score-=3
             alien_lazer.remove(laser)
 
-
+    
         elif laser.y > HEIGHT:
             alien_lazer.remove(laser)
         
+    if is_boss:
+        boss.y += 5
+
+    """ if final_boss():
+        remove.alien_fleet()"""
     
     space_craft_wallcollision()
     if not alien_troops:
@@ -110,5 +135,5 @@ def update():
         space_craft.x += 5
     if keyboard.a:
         space_craft.x -= 5   
-
+clock.schedule(final_boss,5.0)
 pgzrun.go()
