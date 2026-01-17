b6e8a763b4b41df1c0af2db462a4f93ffe65c361
oyewalemarvellous <marvellousoyewale64@gmail.com>
2026-01-17 15:12:06 +0000

    Add files via upload

--- Ball_class.py
+++ Ball_class.py
@@ -1,6 +1,8 @@
 import pgzrun
+import random
 
 WIDTH,HEIGHT= 600,300
+point = []
 class Ball:
     def __init__(self,x,y):
         self.x = x
@@ -12,19 +14,27 @@ class Ball:
     def draw_ball(self):
         pos = (self.x, self.y)
         screen.draw.filled_circle(pos,self.radius,"blue")
+        
 
 ball = Ball(50,150)
 def draw():
-    screen.clear()
+    for pointer in point:
+        pointer.draw()
+
     ball.draw_ball()
+   
 speed = -5
 
+def pointer_position():
+    pointer = Actor("crop")
+    pointer.x = 600
+    pointer.y = random.randint(10,280)
+    point.append(pointer)
 def on_key_down(key):
     if key == keys.SPACE:
         ball.y  -= 25
     
-    
-
+pointer_position()
 def update():
     global speed
     #Bouncing ball
@@ -34,7 +44,12 @@ def update():
     elif ball.y == 550:
         speed = -5"""
     if  ball.y <= 280:
-        ball.y += 1
+        ball.y += 1 
+    #pointer 
 
+    for pointer in point[:]:
+        pointer.x -= 5
+        if pointer.x < WIDTH:
+            point.remove(pointer)
 
 pgzrun.go()
